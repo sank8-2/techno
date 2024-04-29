a7927dba6f488b61b07c3c8d9cd7c70d3858d093
Sanketh Kumar <sankethkumarbg1995@gmail.com>
2024-04-29 22:50:54 +0530

    Size chota kia

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -37,7 +37,7 @@ const Schedule = () => {
         <Stack marginBottom={10} alignItems="center">
           <Table
             border={"2px"}
-            width={["100%", "60%"]}
+            width={["100%", "50%"]}
             variant="striped"
             colorScheme="blackAlpha"
           >
